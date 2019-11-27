a3c0292cbe7a269037d6e305ab1b576cf7cacd4e
marcelpinto <marcelpinto@google.com>
2019-11-27 17:22:35 +0800

    Update dependencies and use workaround to test DFM.
    Basically adding the failing dependency as androidTestImplementation
    to every DFM.

--- DynamicFeatures/app/src/androidTest/java/com/google/android/samples/dynamicfeatures/MainActivityTest.kt
+++ DynamicFeatures/app/src/androidTest/java/com/google/android/samples/dynamicfeatures/MainActivityTest.kt
@@ -20,8 +20,8 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -33,7 +33,7 @@ import org.junit.runner.RunWith
 class MainActivityTest {
 
     @get:Rule
-    val rule = ActivityScenarioRule<MainActivity>(MainActivity::class.java)
+    val rule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
 
     @Test
     fun launchMainActivity() {
